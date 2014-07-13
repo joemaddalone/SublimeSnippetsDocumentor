3afde7a5f87dc5cb08a75c2ae7b33256896c09c1
Joe Maddalone <joe@insert-title.com>
2014-07-12 22:11:33 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,115 @@
+#!/usr/bin/env node
+
+'use strict';
+var nomnom    = require( 'nomnom' ),
+    fs          = require( 'fs' ),
+    xml2js      = require( 'xml2js' ),
+    easyzip     = require( 'easy-zip' ).EasyZip;
+
+var parser = new xml2js.Parser(),
+    ARGS       = nomnom.parse(),
+    dir        = ARGS.d || process.cwd(),  // optional source directory
+    md         = ARGS.md || 'README', // optional markdown filename
+    p          = ARGS.p;  // optional package name
+
+
+var walk = function ( dir, done ) {
+  var results = [];
+
+  fs.readdir( dir, function ( err, list ) {
+    if ( err ) return done( err );
+    var i = 0;
+
+    (function next() {
+
+      var file = list[ i++ ];
+      if ( !file ) return done( null, results );
+
+      file = dir + '/' + file;
+
+      fs.stat( file, function ( err, stat ) {
+
+        if ( stat && stat.isDirectory() ) {
+
+          walk( file, function ( err, res ) {
+
+            results = results.concat( res );
+
+            next();
+
+          });
+
+        } else {
+
+          if(file.indexOf('sublime-snippet') > -1){
+
+            results.push( file );
+
+          }
+
+          next();
+
+        }
+      } );
+    } )();
+  } );
+};
+
+var walker = function( dir, md ){
+  walk( dir, function ( err, results ) {
+    if ( err ) throw err;
+
+    results.map( function ( file ) {
+
+      fs.readFile( file, 'utf-8', function ( err, data ) {
+
+        parser.parseString( data, function ( err, data ) {
+
+          fs.appendFile( md + '.md',
+
+            '**' + data.snippet.tabTrigger + '** | ' +
+
+            data.snippet.description + '\n',
+
+            function ( err ) {
+              // do nothing
+            } );
+
+        } );
+
+      } );
+
+    } );
+
+    console.log('PROCESS COMPLETED!');
+
+  } );
+};
+
+
+
+if( dir ) {
+
+  console.log('Compiling %s', dir);
+
+  if( p ){
+    var zip = new easyzip();
+    zip.zipFolder( dir, function(){
+      zip.writeToFile( p + '.zip' );
+      console.log( 'Rendered Package' );
+    } );
+  }
+
+
+  fs.writeFile( md + '.md', 'Snippets for Sublime Text' +
+    '\n=================================\n' +
+    'Trigger | Description\n' +
+    ':------- | :-------\n'
+  );
+
+  walker(dir, md);
+
+} else {
+
+  console.log('yo, you need a directory, son...');
+}
